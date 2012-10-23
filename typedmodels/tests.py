0108c9af93d11f550332157abd6ca218bc21ed9b
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2012-10-23 13:00:29 +0200

    Moved tests to typedmodels package.

--- typedmodels/tests.py
+++ typedmodels/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from myapp.models import AngryBigCat, Animal, BigCat, Canine, Feline
+from .test_models import AngryBigCat, Animal, BigCat, Canine, Feline
 
 
 class SetupStuff(TestCase):
@@ -18,7 +18,7 @@ class TestTypedModels(SetupStuff):
         self.assertRaises(RuntimeError, Animal.objects.create, name="uhoh")
 
         # ... unless a type is specified
-        Animal.objects.create(name="dingo", type="myapp.canine")
+        Animal.objects.create(name="dingo", type="typedmodels.canine")
 
         # ... unless that type is stupid
         try:
@@ -30,32 +30,32 @@ class TestTypedModels(SetupStuff):
         # all objects returned
         qs = Animal.objects.all().order_by('type')
         self.assertEqual(len(qs), 5)
-        self.assertEqual([obj.type for obj in qs], ['myapp.angrybigcat', 'myapp.bigcat', 'myapp.canine', 'myapp.feline', 'myapp.feline'])
+        self.assertEqual([obj.type for obj in qs], ['typedmodels.angrybigcat', 'typedmodels.bigcat', 'typedmodels.canine', 'typedmodels.feline', 'typedmodels.feline'])
         self.assertEqual([type(obj) for obj in qs], [AngryBigCat, BigCat, Canine, Feline, Feline])
 
     def test_proxy_model_queryset(self):
         qs = Canine.objects.all()
         self.assertEqual(qs.count(), 1)
         self.assertEqual(len(qs), 1)
-        self.assertEqual([obj.type for obj in qs], ['myapp.canine'])
+        self.assertEqual([obj.type for obj in qs], ['typedmodels.canine'])
         self.assertEqual([type(obj) for obj in qs], [Canine])
 
         qs = Feline.objects.all()
         self.assertEqual(qs.count(), 4)
         self.assertEqual(len(qs), 4)
-        self.assertEqual([obj.type for obj in qs], ['myapp.angrybigcat', 'myapp.bigcat', 'myapp.feline', 'myapp.feline'])
+        self.assertEqual([obj.type for obj in qs], ['typedmodels.angrybigcat', 'typedmodels.bigcat', 'typedmodels.feline', 'typedmodels.feline'])
         self.assertEqual([type(obj) for obj in qs], [AngryBigCat, BigCat, Feline, Feline])
 
     def test_doubly_proxied_model_queryset(self):
         qs = BigCat.objects.all()
         self.assertEqual(qs.count(), 2)
         self.assertEqual(len(qs), 2)
-        self.assertEqual([obj.type for obj in qs], ['myapp.angrybigcat', 'myapp.bigcat'])
+        self.assertEqual([obj.type for obj in qs], ['typedmodels.angrybigcat', 'typedmodels.bigcat'])
         self.assertEqual([type(obj) for obj in qs], [AngryBigCat, BigCat])
 
     def test_triply_proxied_model_queryset(self):
         qs = AngryBigCat.objects.all()
         self.assertEqual(qs.count(), 1)
         self.assertEqual(len(qs), 1)
-        self.assertEqual([obj.type for obj in qs], ['myapp.angrybigcat'])
+        self.assertEqual([obj.type for obj in qs], ['typedmodels.angrybigcat'])
         self.assertEqual([type(obj) for obj in qs], [AngryBigCat])
