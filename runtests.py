b693cd78e91144a0e1dbe2554c00af382dc5d68e
Craig de Stigter <craig.destigter@koordinates.com>
2015-09-28 09:10:46 +1300

    Remove django <1.7 check from runtests

--- runtests.py
+++ runtests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import django
 from django.conf import settings
+
 settings.configure(
     INSTALLED_APPS=('typedmodels',),
     MIDDLEWARE_CLASSES=(),
@@ -7,12 +9,11 @@ settings.configure(
         'default': {'ENGINE': 'django.db.backends.sqlite3'}
     })
 
+django.setup()
+
 from django.test.utils import setup_test_environment
 setup_test_environment()
 
-import django
-if django.VERSION > (1, 7):
-    django.setup()
 
 from django.core.management import call_command
 call_command('test', 'typedmodels')
